e3ea8f084c945f724430bfb8abbc6e3573e162ff
wendler <wendler_mendes@hotmail.com>
2021-08-08 11:58:55 -0300

    refact: add pipe operator

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,11 +1,14 @@
 defmodule ListFilter do
-  def call(list),
-    do: Enum.count(remove_not_numbers(list), fn element -> rem(element, 2) > 0 end)
-
-  defp remove_not_numbers(list) do
-    only_numbers_list = Enum.filter(list, fn element -> String.match?(element, ~r/[0-9]/) end)
-    Enum.map(only_numbers_list, fn element -> String.to_integer(element) end)
+  def call(list) do
+    list |> remove_not_numbers() |> string_to_integer() |> count_odd_numbers()
   end
-end
 
-# Use Enum.filter(fn str -> String.match?(str, ~r/[0-9]+/) end) para retirar os itens não numéricos da lista antes de transformá-los em inteiros. Ao utilizar este filtro, ficarão na lista apenas o que contêm números.
+  def remove_not_numbers(list),
+    do: Enum.filter(list, fn element -> String.match?(element, ~r/[0-9]/) end)
+
+  def string_to_integer(list),
+    do: Enum.map(list, fn element -> String.to_integer(element) end)
+
+  def count_odd_numbers(list),
+    do: Enum.count(list, fn element -> rem(element, 2) > 0 end)
+end
