6a1979555959731788b52e456ba596bbd49a2c3b
wendler <wendler_mendes@hotmail.com>
2021-08-08 12:14:48 -0300

    refact: remove separated functions

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,14 +1,8 @@
 defmodule ListFilter do
   def call(list) do
-    list |> remove_not_numbers() |> string_to_integer() |> count_odd_numbers()
+    list
+    |> Enum.filter(fn element -> String.match?(element, ~r/[0-9]/) end)
+    |> Enum.map(fn element -> String.to_integer(element) end)
+    |> Enum.count(fn element -> rem(element, 2) > 0 end)
   end
-
-  def remove_not_numbers(list),
-    do: Enum.filter(list, fn element -> String.match?(element, ~r/[0-9]/) end)
-
-  def string_to_integer(list),
-    do: Enum.map(list, fn element -> String.to_integer(element) end)
-
-  def count_odd_numbers(list),
-    do: Enum.count(list, fn element -> rem(element, 2) > 0 end)
 end
